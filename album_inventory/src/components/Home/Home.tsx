ae71f102b5d720b409f296b862fd4b77b83ddc6b
ethanjharrington012 <ethanjharrington012@gmail.com>
2023-05-01 21:40:03 -0700

    first commit

---
+++ album_inventory/src/components/Home/Home.tsx
@@ -0,0 +1,93 @@
+import React from 'react';
+
+import { styled, width } from '@mui/system';
+import { Button } from '@mui/material';
+import album_image from '../../assets/images/album.jpg';
+import { Link } from 'react-router-dom';
+
+interface Props{
+    title: string;
+}
+
+//...interface above
+
+// Create Styled Components with styled-components
+const Root = styled("div")({
+    padding: 0,
+    margin: 0
+})
+const NavbarContainer = styled('div')( {
+    display: 'flex',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+})
+const Logo = styled('h1')({
+    margin: '0 0 0 0.45em'
+})
+const LogoA = styled('a')( {
+    color: 'rgb(28,24,22)',
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none'
+})
+const LogoNavigation = styled('ul')( {
+    listStyle: 'none',
+    textTransform: 'uppercase',
+    textDecoration: 'none',
+    display: 'flex'
+})
+
+const NavA = styled(Link)({
+    display: 'block',
+    padding: '1em',
+    color: 'black'
+})
+const Main = styled('main')( {
+    backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${album_image});`,
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    position: 'absolute',
+})
+const MainText = styled('div')({
+    textAlign: 'center',
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    color: 'white'
+})
+
+export const Home = ( props:Props) => {
+ 
+    return (
+        <Root>
+            <NavbarContainer>
+                <Logo>
+                    <LogoA href="#">Brand</LogoA>
+                </Logo>
+                <LogoNavigation>
+                    <li>
+                        <NavA to='/'>Home</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/dashboard'>About</NavA>
+                    </li>
+                    <li>
+                        <NavA to='/signin'>Learn More</NavA>
+                    </li>
+                </LogoNavigation>
+            </NavbarContainer>
+            <Main>
+                <MainText>
+                    <h1>{props.title}</h1>
+                    <p>for those who love music</p>
+                    <Button color='primary' variant='contained' component={Link} to='/dashboard'>See the Drones</Button>
+                </MainText>
+            </Main>
+
+        </Root>
+    )
+}
---
+++ album_inventory/src/index.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import './styles.css'
+// Import From react-router-dom & Components
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Home,Dashboard,SignIn } from './components';
+
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+   <Router>
+    <Routes>
+    <Route path='/' element={<Home title  ={'Album Inventory'}/>} />
+    <Route path='/dashboard' element={<Dashboard />} />
+    <Route path='/signin' element={<SignIn />} />
+    </Routes>
+   </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
